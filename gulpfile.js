a06f78a592bbd0ab121b289a0e586b9960f1a3e4
Goce Mitevski <goce.mitevski@keitaro.com>
2017-06-22 16:15:25 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,17 +6,17 @@ var sourcemaps = require('gulp-sourcemaps');
 gulp.task('default', ['less', 'watch']);
 
 gulp.task('less', function () {
-    return gulp.src('assets/less/**/style.less')
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(cleanCSS({
+  return gulp.src('assets/less/**/style.less')
+          .pipe(sourcemaps.init())
+          .pipe(less())
+          .pipe(cleanCSS({
             compatibility: 'ie8'
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('assets/css'));
-})
+          }))
+          .pipe(sourcemaps.write())
+          .pipe(gulp.dest('assets/css'));
+});
 
 gulp.task('watch', function () {
-    gulp.watch(['assets/**/*.less'], ['less']);
-    // gulp.watch(['assets/js/inc/*.js'], ['js']);
+  gulp.watch(['assets/**/*.less'], ['less']);
+  // gulp.watch(['assets/js/inc/*.js'], ['js']);
 });
