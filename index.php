f394f3f3b56560ca5b3f5faa4dee5cd7b6ba2bc1
Goce Mitevski <goce.mitevski@keitaro.com>
2017-06-22 17:49:55 +0200

    Update <header>

--- index.php
+++ index.php
@@ -1,43 +1,36 @@
 <?php get_header(); ?>
 
 <div class="container">
-  <?php if (is_home() && is_front_page()) : ?>
-    <header>
-      <h1 class="page-title"><?php single_post_title(); ?></h1>
-    </header>
-  <?php else : ?>
-    <header class="page-header">
-      <h2 class="page-title"><?php _e('Posts', 'twentyseventeen'); ?></h2>
-    </header>
-  <?php endif; ?>
+    <?php if (!is_front_page()) : ?>
+        <header class="page-header">
+            <h1 class="page-title"><?php single_post_title(); ?></h1>
+        </header>
+    <?php endif; ?>
 
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-      <?php
-      if (have_posts()) :
-        /* Start the Loop */
-        while (have_posts()) :
-          the_post();
+            <?php
+            if (have_posts()) :
+                /* Start the Loop */
+                while (have_posts()) :
+                    the_post();
 
-        /*
-         * Include the Post-Format-specific template for the content.
-         * If you want to override this in a child theme, then include a file
-         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-         */
-        endwhile;
+                /*
+                 * Include the Post-Format-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                endwhile;
 
-      else :
-      endif;
-      ?>
-    </main>
+            else :
+            endif;
+            ?>
+        </main>
 
-  </div>
-  <?php get_sidebar(); ?>
+    </div>
+    <?php get_sidebar(); ?>
 </div>
 
 <?php
-if (is_home()):
-  get_template_part('templates/home/call-to-action');
-endif;
 get_footer();
