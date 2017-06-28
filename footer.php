8cb135c34436cee6bb93ef04e7666c2e3d52e97a
Goce Mitevski <goce.mitevski@keitaro.com>
2017-06-28 10:27:40 +0200

    Use SNIPPETS_DIR with get_template_part()

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php get_template_part('templates/home/call-to-action'); ?>
+<?php get_template_part(SNIPPETS_DIR . '/home/call-to-action'); ?>
 <div class="container">
     <div class="content main-footer">
 
@@ -9,32 +9,27 @@
             </div>
         <?php endif; ?>
         <div class="row">
-
             <div class="col-md-6">
-                <?php get_template_part('templates/navigation/footer', 'menu'); ?>
+                <?php get_template_part(SNIPPETS_DIR . '/navigation/footer', 'menu'); ?>
             </div>
             <div class="col-md-6">
                 <div class="text-right">
-                    <?php get_template_part('templates/navigation/social', 'menu'); ?>
+                    <?php get_template_part(SNIPPETS_DIR . '/navigation/social', 'menu'); ?>
                 </div>
             </div>
-
         </div>
         <hr>
         <div class="row">
-
             <div class="col-md-6">
-                <?php get_template_part('templates/navigation/footer-secondary', 'menu'); ?>
+                <?php get_template_part(SNIPPETS_DIR . '/navigation/footer-secondary', 'menu'); ?>
             </div>
             <div class="col-md-6">
-                <div class="text-right">
-                    <footer class="licenses">
-                        <p>&copy; <?php echo date('Y'); ?> <a href="<?php home_url(); ?>" class="text-uppercase"><?php bloginfo('name'); ?></a>. <?php _e('Some rights reserved.', 'keitaro') ?></p>
-                    </footer>
-                </div>
+                <footer class="copyright">
+                    <p>&copy; <?php echo date('Y'); ?> <a href="<?php home_url(); ?>" class="text-uppercase"><?php bloginfo('name'); ?></a>. <?php _e('Some rights reserved.', 'keitaro') ?></p>
+                </footer>
             </div>
-
         </div>
+
     </div>
     <?php wp_footer(); ?>
 </body>
