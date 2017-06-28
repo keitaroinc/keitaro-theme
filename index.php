7be57c0548aff0615d057f00b194f08fcf46c352
Goce Mitevski <goce.mitevski@keitaro.com>
2017-06-28 12:26:43 +0200

    Replace code blocks with snippets

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <?php if (!is_front_page()) : ?>
-        <header class="page-header">
-            <h1 class="page-title"><?php single_post_title(); ?></h1>
-        </header>
-    <?php endif; ?>
+    <?php // get_template_part(SNIPPETS_DIR . '/header/page', 'header'); ?>
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
@@ -16,14 +12,16 @@
                 while (have_posts()) :
                     the_post();
 
-                /*
-                 * Include the Post-Format-specific template for the content.
-                 * If you want to override this in a child theme, then include a file
-                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                 */
+                    /*
+                     * Include the Post-Format-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                     */
+                    get_template_part(SNIPPETS_DIR . '/content/content');
                 endwhile;
 
             else :
+                get_template_part(SNIPPETS_DIR . '/content/content', 'none');
             endif;
             ?>
         </main>
