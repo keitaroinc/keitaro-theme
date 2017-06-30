60a921db7ee94377ea85aef7f02e73feb426e22c
Goce Mitevski <goce.mitevski@keitaro.com>
2017-06-30 16:28:20 +0200

    Include entry-footer snippet and update page structure in content.php

--- template-snippets/content/content.php
+++ template-snippets/content/content.php
@@ -1,23 +1,30 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    
+
     <div class="row">
-        <div class="col-md-4 text-right">
-            <?php
-            if ('' !== get_the_post_thumbnail() && !is_single()) :
-                get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
-            endif;
-            keitaro_child_pages_list($post->ID);
-            ?>
-        </div>
         <div class="col-md-8">
             <?php
             get_template_part(SNIPPETS_DIR . '/header/entry', 'header');
-            get_template_part(SNIPPETS_DIR . '/entry', 'content');
-            if (is_single()) {
-//		twentyseventeen_entry_footer();
-            }
+
+            if (is_archive()):
+                get_template_part(SNIPPETS_DIR . '/entry', 'excerpt');
+            else:
+                get_template_part(SNIPPETS_DIR . '/entry', 'content');
+            endif;
+            ?>
+        </div>
+        <div class="col-md-4">
+            <?php
+            if (is_archive()):
+            else:
+                if ('' !== get_the_post_thumbnail() && !is_single()) :
+                    get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
+                endif;
+                keitaro_child_pages_list($post->ID);
+
+                get_template_part(SNIPPETS_DIR . '/entry', 'footer');
+            endif;
             ?>
         </div>
     </div>
-    
+
 </article>
