29e3bc5bca2fb42e27cbddd6c660bbd079333ad5
Goce Mitevski <goce.mitevski@keitaro.com>
2017-06-30 16:29:25 +0200

    Add Pages title and code clean

--- template-snippets/entry-content.php
+++ template-snippets/entry-content.php
@@ -1,13 +1,13 @@
 <div class="entry-content">
     <?php
     /* translators: %s: Name of current post */
-    printf(apply_filters('the_content', $post->post_content));
+    echo apply_filters('the_content', $post->post_content);
 //                    the_content(sprintf(
 //                                    __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen'), get_the_title()
 //                    ));
 
     wp_link_pages(array(
-        'before' => '<div class="page-links">' . __('Pages:', 'twentyseventeen'),
+        'before' => '<div class="page-links">' . sprintf('<h3>%s</h3>', __('Page', 'twentyseventeen')),
         'after' => '</div>',
         'link_before' => '<span class="page-number">',
         'link_after' => '</span>',
