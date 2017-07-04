b79e03bf5c750becf4462f1581ad01f790bcf448
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-04 16:35:50 +0200

    Make thumbnail size variable

--- template-snippets/post-thumbnail.php
+++ template-snippets/post-thumbnail.php
@@ -1,5 +1,14 @@
+<?php
+
+if (is_single()):
+    $thumbnail_size = 'large';
+else:
+    $thumbnail_size = 'thumbnail';
+endif;
+
+?>
 <div class="post-thumbnail">
     <a href="<?php the_permalink(); ?>">
-        <?php the_post_thumbnail('thumbnail'); ?>
+        <?php the_post_thumbnail($thumbnail_size); ?>
     </a>
 </div>
