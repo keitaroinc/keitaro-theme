f6734e4f894c7c32087108b0acb3d8c35c33d297
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-04 16:38:21 +0200

    Load different content for pages and posts

--- template-snippets/entry-content.php
+++ template-snippets/entry-content.php
@@ -1,10 +1,11 @@
 <div class="entry-content">
     <?php
-    /* translators: %s: Name of current post */
-    echo apply_filters('the_content', $post->post_content);
-//                    the_content(sprintf(
-//                                    __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen'), get_the_title()
-//                    ));
+
+    if (is_page()):
+        echo apply_filters('the_content', $post->post_content);
+    else:
+        the_content();
+    endif;
 
     wp_link_pages(array(
         'before' => '<div class="page-links">' . sprintf('<h3>%s</h3>', __('Page', 'twentyseventeen')),
@@ -12,5 +13,6 @@
         'link_before' => '<span class="page-number">',
         'link_after' => '</span>',
     ));
+
     ?>
 </div><!-- .entry-content -->
