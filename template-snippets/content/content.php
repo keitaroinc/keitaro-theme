4b5253a53f11b2e695aecfdb4f5d755d07c1b78d
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-04 16:38:54 +0200

    Retab code

--- template-snippets/content/content.php
+++ template-snippets/content/content.php
@@ -3,26 +3,31 @@
     <div class="row">
         <div class="col-md-8">
             <?php
+
             get_template_part(SNIPPETS_DIR . '/header/entry', 'header');
 
             if (is_archive()):
                 get_template_part(SNIPPETS_DIR . '/entry', 'excerpt');
             else:
+                if ('' !== get_the_post_thumbnail()) :
+                    get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
+                endif;
                 get_template_part(SNIPPETS_DIR . '/entry', 'content');
             endif;
+
             ?>
         </div>
         <div class="col-md-4">
             <?php
+
             if (is_archive()):
+            // show something
             else:
-                if ('' !== get_the_post_thumbnail() && !is_single()) :
-                    get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
-                endif;
                 keitaro_child_pages_list($post->ID);
 
-                get_template_part(SNIPPETS_DIR . '/entry', 'footer');
             endif;
+            get_template_part(SNIPPETS_DIR . '/entry', 'footer');
+
             ?>
         </div>
     </div>
