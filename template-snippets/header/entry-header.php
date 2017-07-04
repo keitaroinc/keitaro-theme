cd123906a82ba26176a9322893c34785e7929202
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-04 16:35:13 +0200

    Update entry-header.php

--- template-snippets/header/entry-header.php
+++ template-snippets/header/entry-header.php
@@ -1,20 +1,12 @@
 <?php
+
 if (is_sticky() && is_home()) :
 //		echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
 endif;
+
 ?>
 <header class="entry-header">
     <?php
-    if ('post' === get_post_type()) {
-        echo '<div class="entry-meta">';
-        if (is_single()) {
-//					twentyseventeen_posted_on();
-        } else {
-//					echo twentyseventeen_time_link();
-//					twentyseventeen_edit_link();
-        };
-        echo '</div><!-- .entry-meta -->';
-    };
 
     if (is_single()) {
         the_title('<h1 class="entry-title">', '</h1>');
@@ -23,5 +15,17 @@ endif;
     } else {
         the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
     }
+
+    if ('post' === get_post_type()) {
+        echo '<div class="entry-meta">';
+        if (is_single() || is_archive()) {
+            keitaro_posted_on();
+        } else {
+//					echo twentyseventeen_time_link();
+//					twentyseventeen_edit_link();
+        };
+        echo '</div><!-- .entry-meta -->';
+    };
+
     ?>
 </header>
