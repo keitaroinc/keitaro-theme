b079635a49e1d5c3d62afaa66a7b0cc6458696bf
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-04 16:33:55 +0200

    Add Author and Archive page headers

--- template-snippets/header/page-header.php
+++ template-snippets/header/page-header.php
@@ -1,6 +1,33 @@
-<?php if (!is_front_page()) : ?>
+<?php
+
+$page_title = '';
+
+if (is_author()) :
+    $page_title = __('Content by:', 'keitaro');
+
+elseif (is_archive()):
+    $page_title = single_cat_title(__('Archive:', 'keitaro') . ' ', false);
+endif;
+
+if (!empty($page_title)):
+
+    ?>
     <header class="page-header">
-        <h1 class="page-title"><?php single_post_title(); ?></h1>
+        <div class="row">
+            <div class="col-md-8">
+                <h1 class="page-title"><?php echo $page_title; ?></h1>
+                <?php
+
+                keitaro_posted_on();
+
+                if (is_author()):
+                    keitaro_author_box();
+                endif;
+
+                ?>
+            </div>
+        </div>
     </header>
     <?php
- endif;
+
+endif;
