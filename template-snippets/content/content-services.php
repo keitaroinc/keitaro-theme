8ec3287e03386cdf2f10377d6061ec0d837b60d5
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-07 15:53:59 +0200

    Add dynamic_widget and refactor query code

--- template-snippets/content/content-services.php
+++ template-snippets/content/content-services.php
@@ -1,37 +1,50 @@
 <?php
-global $post;
-$child_pages = get_children(
+
+$child_pages = new WP_Query(
         array(
-            'post_parent' => get_the_ID(),
-            'order' => 'ASC',
-            'orderby' => 'menu_order'
+    'post_parent' => get_the_ID(),
+    'post_type' => 'page',
+    'order' => 'ASC',
+    'orderby' => 'menu_order'
         )
 );
-if ($child_pages):
-    foreach ($child_pages as $page):
 
-        $post = $page;
+if ($child_pages->have_posts()):
+    while ($child_pages->have_posts()):
+        $child_pages->the_post();
+
         ?>
 
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <div class="row">
 
-                <div class="col-md-4 text-right">
-                    <?php
-                    if ('' !== get_the_post_thumbnail() && !is_single()) :
-                        get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
-                    endif;
-                    keitaro_child_pages_list($post->ID);
+                <div class="col-md-4">
+                    <div class="text-right">
+                        <?php
+
+                        if ('' !== get_the_post_thumbnail() && !is_single()) :
+                            get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
+                        endif;
+
+                        keitaro_child_pages_list(get_the_ID());
+
+                        ?>
+                    </div>
+                    <?php dynamic_sidebar('keitaro_page_icon_blocks');
+
                     ?>
                 </div>
                 <div class="col-md-8">
                     <?php
+
                     if (is_sticky() && is_home()) :
 //		echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
                     endif;
+
                     ?>
                     <header class="entry-header">
                         <?php
+
                         if ('post' === get_post_type()) {
                             echo '<div class="entry-meta">';
                             if (is_single()) {
@@ -50,23 +63,21 @@ if ($child_pages):
                         } else {
                             the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
                         }
+
                         ?>
                     </header><!-- .entry-header -->
 
-                    <?php
-                    get_template_part(SNIPPETS_DIR . '/entry', 'content');
-
-                    if (is_single()) {
-//		twentyseventeen_entry_footer();
-                    }
-                    ?>
+        <?php get_template_part(SNIPPETS_DIR . '/entry', 'content'); ?>
 
                 </div>
             </div>
         </article><!-- #post-## -->
         <?php
-        wp_reset_query();
-    endforeach;
+
+    endwhile;
+
+    unset($child_pages);
+
 else:
     get_template_part(SNIPPETS_DIR . '/content/content-services', 'none');
 endif;
