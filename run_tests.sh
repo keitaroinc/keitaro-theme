0bd558e64bf6591f232bcdb2caf898f8e84eadff
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-12 17:18:51 +0200

    Add PHP CodeSniffer tests script

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Search for PHP syntax errors.
+find -L . -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
+
+# WordPress Coding Standards.
+phpcs -p -s -v -n . --standard=./phpcs.xml --extensions=php
+
+# Run the theme JavaScript partials through ESLint
+#eslint -v -c ./.eslintrc.js ./assets/scripts/concat
+
+# Run the theme LESS partials through LESS Lint.
+lesslint assets/less/*.less
