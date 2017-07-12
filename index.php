080daa84e47bc1df79fd2f2cc4ac5972c9970d3a
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-12 17:14:27 +0200

    Lint index.php

--- index.php
+++ index.php
@@ -2,44 +2,46 @@
 
 <div class="container">
 
-    <div id="primary" class="content-area">
-        <?php get_template_part(SNIPPETS_DIR . '/header/page', 'header'); ?>
-        <main id="main" class="site-main" role="main">
+	<?php if (!is_front_page()) : ?>
+		<div id="primary" class="content-area">
+			<?php get_template_part(SNIPPETS_DIR . '/header/page', 'header'); ?>
+			<main id="main" class="site-main" role="main">
 
-            <?php
+				<?php
 
-            if (have_posts()) :
-                /* Start the Loop */
-                while (have_posts()) :
-                    the_post();
+				if (have_posts()) :
+					/* Start the Loop */
+					while (have_posts()) :
+						the_post();
 
-                    /*
-                     * Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    if (is_page()):
-                        get_template_part(SNIPPETS_DIR . '/content/content', 'page');
-                    else:
-                        get_template_part(SNIPPETS_DIR . '/content/content');
-                    endif;
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						if (is_page()) :
+							get_template_part(SNIPPETS_DIR . '/content/content', 'page');
+						else :
+							get_template_part(SNIPPETS_DIR . '/content/content');
+						endif;
 
-                endwhile;
+					endwhile;
 
-            else :
-                get_template_part(SNIPPETS_DIR . '/content/content', 'none');
-            endif;
+				else :
+					get_template_part(SNIPPETS_DIR . '/content/content', 'none');
+				endif;
 
-            echo paginate_links(array(
-                'mid_size' => 4,
-                'type' => 'list'
-            ));
+				echo paginate_links(array(
+					'mid_size' => 4,
+					'type' => 'list',
+				));
 
-            ?>
-        </main>
+				?>
+			</main>
 
-    </div>
-<?php get_sidebar(); ?>
+		</div>
+	<?php endif; ?>
+	<?php get_sidebar(); ?>
 </div>
 
 <?php
