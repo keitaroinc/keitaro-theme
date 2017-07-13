01b816555ae31c07a3678c1035332547acb8502e
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-13 15:30:57 +0200

    Exclude content on the search results template

--- template-snippets/content/content.php
+++ template-snippets/content/content.php
@@ -4,15 +4,15 @@
 		<div class="col-md-8">
 			<?php
 
-			get_template_part( SNIPPETS_DIR . '/header/entry', 'header' );
+			get_template_part(SNIPPETS_DIR . '/header/entry', 'header');
 
-			if ( is_archive() || is_home() ) :
-				get_template_part( SNIPPETS_DIR . '/entry', 'excerpt' );
+			if (is_archive() || is_home() || is_search()) :
+				get_template_part(SNIPPETS_DIR . '/entry', 'excerpt');
 			else :
-				if ( '' !== get_the_post_thumbnail() ) :
-					get_template_part( SNIPPETS_DIR . '/post', 'thumbnail' );
+				if ('' !== get_the_post_thumbnail()) :
+					get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
 				endif;
-				get_template_part( SNIPPETS_DIR . '/entry', 'content' );
+				get_template_part(SNIPPETS_DIR . '/entry', 'content');
 			endif;
 
 			comments_template();
@@ -22,13 +22,15 @@
 		<div class="col-md-4">
 			<?php
 
-			keitaro_child_pages_list( get_the_ID() );
+			if (!is_search()):
+				keitaro_child_pages_list(get_the_ID());
 
-			foreach ( get_children( get_ancestors( get_the_ID() ) ) as $page ) :
-				dynamic_sidebar( 'keitaro_page_icon_blocks' );
-			endforeach;
+				foreach (get_children(get_ancestors(get_the_ID())) as $page) :
+					dynamic_sidebar('keitaro_page_icon_blocks');
+				endforeach;
+			endif;
 
-			get_template_part( SNIPPETS_DIR . '/entry', 'footer' );
+			get_template_part(SNIPPETS_DIR . '/entry', 'footer');
 
 			?>
 		</div>
