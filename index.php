26e0efa04e7e3a03e57f6760a3c6d575fe501fbc
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-13 16:57:19 +0200

    Add Twitter Content dynamic sidebar

--- index.php
+++ index.php
@@ -2,53 +2,65 @@
 
 <div class="container">
 
-	<?php if (!is_front_page()) : ?>
-		<?php
+    <?php if (!is_front_page()) : ?>
+        <?php
 
-		if (!is_404()):
-			get_search_form();
-		endif;
+        if (!is_404()):
+            get_search_form();
+        endif;
 
-		?>
-		<div id="primary" class="content-area">
-				<?php get_template_part(SNIPPETS_DIR . '/header/page', 'header'); ?>
-			<main id="main" class="site-main" role="main">
+        ?>
+        <div id="primary" class="content-area">
+            <?php get_template_part(SNIPPETS_DIR . '/header/page', 'header'); ?>
+            <main id="main" class="site-main" role="main">
 
-				<?php
+                <?php
 
-				if (have_posts()) :
-					/* Start the Loop */
-					while (have_posts()) :
-						the_post();
+                if (have_posts()) :
+                    /* Start the Loop */
+                    while (have_posts()) :
+                        the_post();
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						if (is_page()) :
-							get_template_part(SNIPPETS_DIR . '/content/content', 'page');
-						else :
-							get_template_part(SNIPPETS_DIR . '/content/content');
-						endif;
+                        /*
+                         * Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        if (is_page()) :
+                            get_template_part(SNIPPETS_DIR . '/content/content', 'page');
+                        else :
+                            get_template_part(SNIPPETS_DIR . '/content/content');
+                        endif;
 
-					endwhile;
+                    endwhile;
 
-				else :
-					get_template_part(SNIPPETS_DIR . '/content/content', 'none');
-				endif;
+                else :
+                    get_template_part(SNIPPETS_DIR . '/content/content', 'none');
+                endif;
 
-				echo paginate_links(array(
-					'mid_size' => 6,
-					'type' => 'list',
-				));
+                echo paginate_links(array(
+                    'mid_size' => 6,
+                    'type' => 'list',
+                ));
 
-				?>
-			</main>
+                global $paged;
 
-		</div>
-<?php endif; ?>
-<?php get_sidebar(); ?>
+                if ($paged < 1):
+
+                    ?>
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <?php dynamic_sidebar('keitaro_twitter_content'); ?>
+                        </div>
+                    </div>
+                <?php endif;
+
+                ?>
+            </main>
+
+        </div>
+    <?php endif; ?>
+    <?php get_sidebar(); ?>
 </div>
 
 <?php
