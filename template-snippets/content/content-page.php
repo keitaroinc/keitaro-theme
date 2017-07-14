86a95af11c9990b160a08bbe1469a611c6dcb41d
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-14 13:36:18 +0200

    Include icon-blocks.php sidebar snippet and clean code

--- template-snippets/content/content-page.php
+++ template-snippets/content/content-page.php
@@ -3,8 +3,8 @@
         <div class="col-md-8">
             <?php
 
-            get_template_part(SNIPPETS_DIR . '/header/entry', 'header');
-            get_template_part(SNIPPETS_DIR . '/entry', 'content');
+            get_template_part(SNIPPETS_DIR . '/header/entry-header');
+            get_template_part(SNIPPETS_DIR . '/entry-content');
 
             comments_template();
 
@@ -14,7 +14,7 @@
             <?php
 
             if ('' !== get_the_post_thumbnail() && !is_single()) :
-                get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
+                get_template_part(SNIPPETS_DIR . '/post-thumbnail');
             endif;
 
             keitaro_child_pages_list(get_the_ID());
