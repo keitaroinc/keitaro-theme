7a98dbfa33459ac60c1af346c08fbe41a350cd2b
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-14 13:33:28 +0200

    Load snippet only when page content not empty and clean code

--- template-snippets/content/content-parent-page.php
+++ template-snippets/content/content-parent-page.php
@@ -1,27 +1,11 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="row">
-		<div class="col-md-8">
-			<?php
+<?php if (get_the_content()) : ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php
 
-			get_template_part( SNIPPETS_DIR . '/header/entry', 'header' );
-			get_template_part( SNIPPETS_DIR . '/entry', 'content' );
+        get_template_part(SNIPPETS_DIR . '/header/entry-header');
+        get_template_part(SNIPPETS_DIR . '/entry-content');
 
-			comments_template();
-
-			?>
-		</div>
-		<div class="col-md-4">
-			<?php
-
-			if ( '' !== get_the_post_thumbnail() && ! is_single() ) :
-				get_template_part( SNIPPETS_DIR . '/post', 'thumbnail' );
-			endif;
-
-			keitaro_child_pages_list( get_the_ID() );
-
-			dynamic_sidebar( 'keitaro_page_icon_blocks' );
-
-			?>
-		</div>
-	</div>
-</article>
+        ?>
+    </article>
+    <?php
+endif;
