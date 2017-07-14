5d6565d479a920b35834cddf759912d1ba57bf9e
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-14 16:48:45 +0200

    Update CSS classes

--- template-snippets/header/hero.php
+++ template-snippets/header/hero.php
@@ -1,7 +1,7 @@
 <div class="hero">
   <div class="container">
     <div class="row">
-      <div class="col-md-7">
+      <div class="col-md-8 col-lg-7">
         <?php do_shortcode( '[keitaro-hero-title]' ); ?>
 	  </div>
 	</div>
