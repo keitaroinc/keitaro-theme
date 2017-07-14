d60b08f60c1d81aac9baf2685c67a362c4dac22f
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-14 13:31:37 +0200

    Include icons-blocks sidebar snippet and code clean

--- template-snippets/content/content.php
+++ template-snippets/content/content.php
@@ -1,39 +1,39 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="row">
-		<div class="col-md-8">
-			<?php
+    <div class="row">
+        <div class="col-md-8">
+            <?php
 
-			get_template_part(SNIPPETS_DIR . '/header/entry', 'header');
+            get_template_part(SNIPPETS_DIR . '/header/entry-header');
 
-			if (is_archive() || is_home() || is_search()) :
-				get_template_part(SNIPPETS_DIR . '/entry', 'excerpt');
-			else :
-				if ('' !== get_the_post_thumbnail()) :
-					get_template_part(SNIPPETS_DIR . '/post', 'thumbnail');
-				endif;
-				get_template_part(SNIPPETS_DIR . '/entry', 'content');
-			endif;
+            if (is_archive() || is_home() || is_search()) :
+                get_template_part(SNIPPETS_DIR . '/entry-excerpt');
+            else :
+                if ('' !== get_the_post_thumbnail()) :
+                    get_template_part(SNIPPETS_DIR . '/post-thumbnail');
+                endif;
+                get_template_part(SNIPPETS_DIR . '/entry-content');
+            endif;
 
-			comments_template();
+            comments_template();
 
-			?>
-		</div>
-		<div class="col-md-4">
-			<?php
+            ?>
+        </div>
+        <div class="col-md-4">
+            <?php
 
-			if (!is_search()):
-				keitaro_child_pages_list(get_the_ID());
+            if (!is_search()):
+                keitaro_child_pages_list(get_the_ID());
 
-				foreach (get_children(get_ancestors(get_the_ID())) as $page) :
-					dynamic_sidebar('keitaro_page_icon_blocks');
-				endforeach;
-			endif;
+                foreach (get_children(get_ancestors(get_the_ID())) as $page) :
+                    get_template_part(SNIPPETS_DIR . '/sidebars/icon-blocks');
+                endforeach;
+            endif;
 
-			get_template_part(SNIPPETS_DIR . '/entry', 'footer');
+            get_template_part(SNIPPETS_DIR . '/entry-footer');
 
-			?>
-		</div>
-	</div>
+            ?>
+        </div>
+    </div>
 
 </article>
