bbbfc7137e3cc5c7a119d95ce3e155a60b84dcbf
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-18 16:24:05 +0200

    Add parent.php

---
+++ parent.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Template Name: Parent Page
+ */
+get_header();
+
+?>
+
+<div class="container">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+            <?php
+
+            if (have_posts()) :
+                /* Start the Loop */
+                while (have_posts()) :
+                    the_post();
+
+                    get_template_part(SNIPPETS_DIR . '/content/content-parent-page');
+                    get_template_part(SNIPPETS_DIR . '/content/content-children');
+
+                endwhile;
+            else :
+                get_template_part(SNIPPETS_DIR . '/content/content-none');
+            endif;
+
+            ?>
+        </main>
+
+    </div>
+</div>
+
+<?php
+
+get_sidebar();
+
+get_footer();
