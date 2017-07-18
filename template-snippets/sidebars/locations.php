5464bee53ad6df77bc8e67a28dc13f8d4cd87205
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-18 16:34:09 +0200

    Add locations.php sidebar snippet

---
+++ template-snippets/sidebars/locations.php
@@ -0,0 +1,13 @@
+<?php
+
+$sidebar_id = 'keitaro_locations';
+
+if (is_active_sidebar($sidebar_id)):
+
+    ?>
+    <div class="locations">
+        <?php dynamic_sidebar($sidebar_id); ?>
+    </div>
+    <?php
+
+endif;
