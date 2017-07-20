358181bc300783404fbc4119c9d4d2ccce5d7d7b
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-20 17:21:43 +0200

    Add content for search results title and subtitle

--- template-snippets/header/page-header.php
+++ template-snippets/header/page-header.php
@@ -1,33 +1,95 @@
 <?php
 
 $page_title = '';
+$page_subtitle = '';
 
-if ( is_author() ) :
-	$page_title = __( 'Content by:', 'keitaro' );
+if (is_author()) :
+    $page_title = __('Content by:', 'keitaro');
 
-elseif ( is_archive() ) :
-	$page_title = single_cat_title( __( 'Archive:', 'keitaro' ) . ' ', false );
+elseif (is_search()):
+    global $wp_query;
+    $page_title = __('Search results:', 'keitaro') . ' ' . highlight(get_search_query());
+    $page_subtitle = __('Found', 'keitaro') . ' ' . highlight($wp_query->found_posts) . ' ' . __('search results', 'keitaro');
+
+elseif (is_archive()) :
+    $page_title = single_cat_title(__('Archive:', 'keitaro') . ' ', false);
 endif;
 
-if ( ! empty( $page_title ) ) :
+if (!empty($page_title)) :
 
-	?>
-	<header class="page-header">
-		<div class="row">
-			<div class="col-md-8">
-				<h1 class="page-title"><?php echo $page_title; ?></h1>
-				<?php
+    ?>
+    <header class="page-header">
+        <div class="row">
+            <div class="col-md-8">
+                <?php if ($page_title): ?>
+                    <h1 class="page-title"><?php echo $page_title; ?></h1>
+                <?php endif; ?>
+                <?php if ($page_subtitle): ?>
+                    <p class="lead"><?php echo $page_subtitle; ?></p>
+                    <?php
 
-				keitaro_posted_on();
+                endif;
 
-				if ( is_author() ) :
-					keitaro_author_box();
-				endif;
+                keitaro_posted_on();
 
-				?>
-			</div>
-		</div>
-	</header>
-	<?php
+                if (is_author()) :
+                    keitaro_author_box();
+                endif;
+
+                ?>
+            </div>
+        </div>
+    </header>
+    <?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 endif;
