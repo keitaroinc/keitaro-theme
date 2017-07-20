36630e8cbd08f3ad93f553cd26a40fc03a6e2780
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-20 17:30:56 +0200

    Add bootstrap-js and bootstrap-fonts tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,35 @@ var less = require('gulp-less');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['less', 'watch']);
+// The default Gulp.js task
+gulp.task('default', ['bootstrap-fonts', 'bootstrap-js', 'less', 'watch']);
 
+// Rebuild CSS from LESS
 gulp.task('less', function () {
-  return gulp.src('assets/less/**/style.less')
-          .pipe(sourcemaps.init())
-          .pipe(less())
-          .pipe(cleanCSS({
-            compatibility: 'ie8'
-          }))
-          .pipe(sourcemaps.write())
-          .pipe(gulp.dest('assets/css'));
+    return gulp.src('assets/less/**/style.less')
+            .pipe(sourcemaps.init())
+            .pipe(less())
+            .pipe(cleanCSS({
+                compatibility: 'ie8'
+            }))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('assets/css'));
 });
 
+// Copy Bootstrap js assets in assets/js
+gulp.task('bootstrap-js', function () {
+    return gulp.src('node_modules/bootstrap/dist/js/bootstrap.min.js')
+            .pipe(gulp.dest('assets/js'));
+});
+
+// Copy Bootstrap font files in assets/fonts
+gulp.task('bootstrap-fonts', function () {
+    return gulp.src('node_modules/bootstrap/dist/fonts/*')
+            .pipe(gulp.dest('assets/fonts/'));
+});
+
+// Watch for LESS and JS file changes
 gulp.task('watch', function () {
-  gulp.watch(['assets/**/*.less'], ['less']);
-  // gulp.watch(['assets/js/inc/*.js'], ['js']);
+    gulp.watch(['assets/**/*.less'], ['less']);
+    // gulp.watch(['assets/js/*.js'], ['js']);
 });
