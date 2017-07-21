90ccf851d04473b76a35368ce963c0768b13fec9
Goce Mitevski <goce.mitevski@keitaro.com>
2017-07-21 16:11:31 +0200

    Rename variables and clean code

--- template-snippets/widgets/class-wp-widget-contact-form.php
+++ template-snippets/widgets/class-wp-widget-contact-form.php
@@ -32,11 +32,11 @@ class Keitaro_Contact_Form extends WP_Widget {
             $email_sent = false;
             $autoreply_sent = false;
 
-            $to = 'goce.mitevski@keitaro.com';
+            $send_to = !empty($instance['sent_to']) ? $instance['sent_to'] : get_option('admin_email');
             $subject = __('New message from Keitaro.com', 'keitaro');
             $subject_autoreply = __('Thank you for contacting Keitaro Inc.', 'keitaro');
-            $company = (isset($_POST['company-name'])) ? trim(esc_html($_POST['company-name'])) : '';
-            $company_email = (isset($_POST['business-email'])) ? trim(esc_html($_POST['business-email'])) : '';
+            $sender = (isset($_POST['sender-name'])) ? trim(esc_html($_POST['sender-name'])) : '';
+            $sender_email = (isset($_POST['sender-email'])) ? trim(esc_html($_POST['sender-email'])) : '';
             $intent = (isset($_POST['intent'])) ? trim(esc_html($_POST['intent'])) : '';
             $submitted_message = (isset($_POST['message'])) ? str_replace("\r\n", "<br>", trim(esc_html($_POST['message']))) : '';
 
@@ -46,7 +46,7 @@ class Keitaro_Contact_Form extends WP_Widget {
             );
             $body = join("<br>", array(
                 __('Hello,', 'keitaro') . "<br>",
-                sprintf(__('%1$s submitted the following message from %2$s though the contact form on %3$s', 'keitaro'), $company, $company_email, get_permalink()) . "<br>",
+                sprintf(__('%1$s submitted the following message from %2$s though the contact form on %3$s', 'keitaro'), $sender, $sender_email, get_permalink()) . "<br>",
                 sprintf(__('The intent is: %s', 'keitaro'), str_replace('-', ' ', ucwords($intent))) . "<br>",
                 $submitted_message . "<br>",
                 __('Regards,', 'keitaro'),
@@ -57,20 +57,22 @@ class Keitaro_Contact_Form extends WP_Widget {
                 __('Hello,', 'keitaro') . "<br>",
                 __('Thank you for contacting us at Keitaro Inc. We are just reaching out to confirm that we received your message and will respond as soon as possible.', 'keitaro') . "<br>",
                 __('Kind Regards,', 'keitaro'),
-                __('Keitaro Inc.', 'keitaro')
+                __('Keitaro Inc.', 'keitaro') . "<br>",
+                esc_url(get_home_url()),
+                $send_to,
             ));
 
             try {
 
                 // Send mail to Keitaro Inc.
-                if (wp_mail($to, $subject, $body, $headers)) :
+                if (wp_mail($send_to, $subject, $body, $headers)) :
                     $email_sent = true;
                 else:
                     throw new Exception(__("Something's wrong. The email message was not sent to Keitaro Inc.", 'keitaro'));
                 endif;
 
                 // Send autoreply to sender
-                if (wp_mail($company_email, $subject_autoreply, $body_autoreply, $headers)) :
+                if (wp_mail($sender_email, $subject_autoreply, $body_autoreply, $headers)) :
                     $autoreply_sent = true;
                 else:
                     throw new Exception(__("Something's wrong. The auto respond email message was not sent to sender.", 'keitaro'));
@@ -112,8 +114,8 @@ class Keitaro_Contact_Form extends WP_Widget {
 
                 <?php if (!empty($instance['name_label'])) : ?>            
                     <div class="form-group">
-                        <input class="form-control" type="text" name="company-name" id="company-name" required="required" value="<?php echo (isset($_POST['company-name']) ? esc_attr($_POST['company-name']) : '') ?>">
-                        <label for="company-name"><?php echo $instance['name_label']; ?></label>
+                        <input class="form-control" type="text" name="sender-name" id="sender-name" required="required" value="<?php echo (isset($_POST['sender-name']) ? esc_attr($_POST['sender-name']) : '') ?>">
+                        <label for="sender-name"><?php echo $instance['name_label']; ?></label>
                     </div>
                     <?php
 
@@ -123,8 +125,8 @@ class Keitaro_Contact_Form extends WP_Widget {
 
                     ?>
                     <div class="form-group">
-                        <input class="form-control" type="email" name="business-email" id="business-email" required="required" value="<?php echo (isset($_POST['business-email']) ? esc_attr($_POST['business-email']) : '') ?>">
-                        <label for="business-email"><?php echo $instance['email_label']; ?></label>
+                        <input class="form-control" type="email" name="sender-email" id="sender-email" required="required" value="<?php echo (isset($_POST['sender-email']) ? esc_attr($_POST['sender-email']) : '') ?>">
+                        <label for="sender-email"><?php echo $instance['email_label']; ?></label>
                     </div>
                     <?php
 
@@ -190,6 +192,7 @@ class Keitaro_Contact_Form extends WP_Widget {
         $email_label = !empty($instance['email_label']) ? $instance['email_label'] : __('Business email', 'keitaro');
         $intent_list = !empty($instance['intent_list']) ? $instance['intent_list'] : __('Intent...', 'keitaro');
         $message_label = !empty($instance['message_label']) ? $instance['message_label'] : __('How may we help you...', 'keitaro');
+        $send_to = !empty($instance['sent_to']) ? $instance['sent_to'] : get_option('admin_email');
         $submit_label = !empty($instance['submit_label']) ? $instance['submit_label'] : __('Contact us', 'keitaro');
         $thank_you = !empty($instance['thank_you']) ? $instance['thank_you'] : __("Thank you for contacting us. You'll hear from us shortly.", 'keitaro');
 
@@ -213,6 +216,10 @@ class Keitaro_Contact_Form extends WP_Widget {
         <p>
             <label for="<?php echo esc_attr($this->get_field_id('intent_list')); ?>"><?php esc_attr_e('Intent list options &mdash; one option per line:', 'keitaro'); ?></label>
             <textarea id="<?php echo esc_attr($this->get_field_id('intent_list')); ?>" name="<?php echo esc_attr($this->get_field_name('intent_list')); ?>" class="widefat text" style="height: 200px" rows="16" cols="20"><?php echo esc_textarea($intent_list); ?></textarea>
+        </p>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('sent_to')); ?>"><?php esc_attr_e('Send email messages to:', 'keitaro'); ?></label>
+            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('sent_to')); ?>" name="<?php echo esc_attr($this->get_field_name('sent_to')); ?>" type="text" value="<?php echo esc_attr($send_to); ?>">
         </p>
         <p>
             <label for="<?php echo esc_attr($this->get_field_id('message_label')); ?>"><?php esc_attr_e('Message field label:', 'keitaro'); ?></label>
@@ -250,6 +257,7 @@ class Keitaro_Contact_Form extends WP_Widget {
         $instance['email_label'] = (!empty($new_instance['email_label']) ) ? strip_tags($new_instance['email_label']) : '';
         $instance['intent_list'] = (!empty($new_instance['intent_list']) ) ? strip_tags($new_instance['intent_list']) : '';
         $instance['message_label'] = (!empty($new_instance['message_label']) ) ? strip_tags($new_instance['message_label']) : '';
+        $instance['sent_to'] = (!empty($new_instance['sent_to']) ) ? strip_tags($new_instance['sent_to']) : '';
         $instance['submit_label'] = (!empty($new_instance['submit_label']) ) ? strip_tags($new_instance['submit_label']) : '';
         $instance['thank_you'] = (!empty($new_instance['thank_you']) ) ? strip_tags($new_instance['thank_you']) : '';
 
