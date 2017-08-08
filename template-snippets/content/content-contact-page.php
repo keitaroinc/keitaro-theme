1375c24e60624176f5f004c48e7ffd0f2eb40a77
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-08 11:54:51 +0200

    Update contact-form.php sidebar

--- template-snippets/content/content-contact-page.php
+++ template-snippets/content/content-contact-page.php
@@ -1,31 +1,3 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="row">
-        <div class="col-md-4">
-            <?php
-
-            if ('' !== get_the_post_thumbnail() && !is_single()) :
-                get_template_part(SNIPPETS_DIR . '/post-thumbnail');
-            endif;
-
-            keitaro_child_pages_list(get_the_ID());
-
-            // Don't show Locations sidebar when contact form data is successfully submitted
-            if (!isset($_POST['submit'])) :
-                get_template_part(SNIPPETS_DIR . '/sidebars/locations');
-            endif;
-
-            ?>
-        </div>
-        <div class="col-md-6">
-            <?php
-
-            if (get_the_content()):
-                get_template_part(SNIPPETS_DIR . '/header/entry-header');
-                get_template_part(SNIPPETS_DIR . '/entry-content');
-            endif;
-            get_template_part(SNIPPETS_DIR . '/sidebars/contact-form');
-
-            ?>
-        </div>
-    </div>
+    <?php get_template_part(SNIPPETS_DIR . '/sidebars/contact-form'); ?>
 </article>
