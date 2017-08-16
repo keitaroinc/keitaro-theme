35154eca71e781cf8a2b906627df31f37ec36547
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-16 13:21:09 +0200

    Add support for number of tweets per widget

--- template-snippets/widgets/class-wp-widget-tweets.php
+++ template-snippets/widgets/class-wp-widget-tweets.php
@@ -41,7 +41,7 @@ class Keitaro_Tweets extends WP_Widget {
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2">
                         <?php endif; ?>
-                        <a class="twitter-<?php echo (!empty($instance['tweets_type']) ? esc_attr($instance['tweets_type']) : ''); ?>" data-lang="en" data-dnt="true" href="<?php echo esc_url($instance['tweets_url']); ?>"><?php echo apply_filters('widget_title', $instance['title']); ?></a>
+                        <a class="twitter-<?php echo (!empty($instance['tweets_type']) ? esc_attr($instance['tweets_type']) : ''); ?>" data-lang="en" data-dnt="true" data-tweet-limit="<?php echo $instance['tweets_limit']; ?>" href="<?php echo esc_url($instance['tweets_url']); ?>"><?php echo apply_filters('widget_title', $instance['title']); ?></a>
                         <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
                         <?php if ($instance['tweets_type'] == $timeline): ?>
                         </div>
@@ -67,6 +67,7 @@ class Keitaro_Tweets extends WP_Widget {
 
         $title = !empty($instance['title']) ? $instance['title'] : '';
         $tweets_url = !empty($instance['tweets_url']) ? $instance['tweets_url'] : '';
+        $tweets_limit = !empty($instance['tweets_limit']) ? $instance['tweets_limit'] : 10;
         $tweets_type = !empty($instance['tweets_type']) ? $instance['tweets_type'] : '';
         $visualization_type = array(
             'grid' => __('Used with timeline URLs', 'keitaro'),
@@ -81,6 +82,10 @@ class Keitaro_Tweets extends WP_Widget {
         <p>
             <label for="<?php echo esc_attr($this->get_field_id('tweets_url')); ?>"><?php esc_attr_e('Twitter URL:', 'keitaro'); ?></label>
             <input class="widefat" id="<?php echo esc_attr($this->get_field_id('tweets_url')); ?>" name="<?php echo esc_attr($this->get_field_name('tweets_url')); ?>" type="text" value="<?php echo esc_attr($tweets_url); ?>">
+        </p>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('tweets_limit')); ?>"><?php esc_attr_e('Tweets shown:', 'keitaro'); ?></label>
+            <input class="widefat" id="<?php echo esc_attr($this->get_field_id('tweets_limit')); ?>" name="<?php echo esc_attr($this->get_field_name('tweets_limit')); ?>" type="number" value="<?php echo esc_attr($tweets_limit); ?>">
         </p>
         <p>
             <label for="<?php echo $this->get_field_id('tweets_type') ?>"><?php esc_attr_e('Show as:', 'keitaro'); ?></label>
@@ -110,6 +115,7 @@ class Keitaro_Tweets extends WP_Widget {
             $instance = $old_instance;
 
             $instance['title'] = (!empty($new_instance['title']) ) ? strip_tags($new_instance['title']) : '';
+            $instance['tweets_limit'] = (!empty($new_instance['tweets_limit']) ) ? strip_tags($new_instance['tweets_limit']) : '';
             $instance['tweets_url'] = (!empty($new_instance['tweets_url']) ) ? strip_tags($new_instance['tweets_url']) : '';
             $instance['tweets_type'] = (!empty($new_instance['tweets_type']) ) ? strip_tags($new_instance['tweets_type']) : '';
 
