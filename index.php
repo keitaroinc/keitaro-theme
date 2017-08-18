cd230c541eb665270bdd99f31df83ee040029237
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-18 16:11:01 +0200

    Restructure index.html to include featured posts

--- index.php
+++ index.php
@@ -19,20 +19,21 @@ if (!is_front_page()) :
                     while (have_posts()) :
                         the_post();
 
-                        /*
-                         * Include the Post-Format-specific template for the content.
-                         * If you want to override this in a child theme, then include a file
-                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                         */
                         if (is_page()) :
                             get_template_part(SNIPPETS_DIR . '/content/content-page');
+                        elseif (is_home() && $paged < 1):
+                            get_template_part(SNIPPETS_DIR . '/content/content-featured');
                         else :
                             get_template_part(SNIPPETS_DIR . '/content/content');
                         endif;
 
                     endwhile;
 
-                    get_template_part(SNIPPETS_DIR . '/navigation/pagination');
+                    if (is_home() && $paged < 1):
+                        keitaro_continue_to_second_blog_posts_page_button(__('Continue', 'keitaro'), get_post_type_archive_link('post'));
+                    else:
+                        get_template_part(SNIPPETS_DIR . '/navigation/pagination');
+                    endif;
 
                     ?>
                 </main>
@@ -40,16 +41,18 @@ if (!is_front_page()) :
                 <?php
 
             else :
+
                 get_template_part(SNIPPETS_DIR . '/content/content-none');
+
             endif;
+
+            get_template_part(SNIPPETS_DIR . '/sidebars/twitter-content');
 
             ?>
         </div>
     </div>
 
     <?php
-
-    get_template_part(SNIPPETS_DIR . '/sidebars/twitter-content');
 
 endif;
 
