1278311bc56341b33b397dbe257e8a8771c92dd7
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-18 16:16:10 +0200

    Add conditional logic for first post in query

--- template-snippets/entry-excerpt.php
+++ template-snippets/entry-excerpt.php
@@ -1,15 +1,30 @@
+<?php
+
+$is_first_post = false;
+
+if ($wp_query->current_post < 1):
+    $is_first_post = true;
+endif;
+
+?>
 <div class="entry-content">
     <?php
-	/* translators: %s: Name of current post */
-	echo apply_filters( 'the_excerpt', get_the_excerpt() );
-    
-    keitaro_read_more();
 
-	wp_link_pages(array(
-		'before' => '<div class="page-links">' . sprintf( '<h3>%s</h3>', __( 'Page', 'twentyseventeen' ) ),
-		'after' => '</div>',
-		'link_before' => '<span class="page-number">',
-		'link_after' => '</span>',
-	));
-	?>
+    /* translators: %s: Name of current post */
+    echo apply_filters('the_excerpt', get_the_excerpt());
+
+    if ($is_first_post):
+        keitaro_read_more('btn-success');
+    else:
+        keitaro_read_more();
+    endif;
+
+    wp_link_pages(array(
+        'before' => '<div class="page-links">' . sprintf('<h3>%s</h3>', __('Page', 'keitaro')),
+        'after' => '</div>',
+        'link_before' => '<span class="page-number">',
+        'link_after' => '</span>',
+    ));
+
+    ?>
 </div><!-- .entry-content -->
