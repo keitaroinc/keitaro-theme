a64804447986fe199bc87f30a8c86119d98c2a20
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-21 12:03:22 +0200

    Make search field input required

--- searchform.php
+++ searchform.php
@@ -4,7 +4,7 @@
     <label class="sr-only" for="<?php echo $unique_id; ?>"><?php echo _x('Search for:', 'label', 'keitaro'); ?></label>
 
     <div class="input-group">
-        <input type="search" id="<?php echo $unique_id; ?>" class="form-control search-field" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'keitaro'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+        <input type="search" id="<?php echo $unique_id; ?>" class="form-control search-field" placeholder="<?php echo esc_attr_x('Search &hellip;', 'placeholder', 'keitaro'); ?>" required="required" value="<?php echo get_search_query(); ?>" name="s" />
         <span class="input-group-btn">
             <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
         </span>
