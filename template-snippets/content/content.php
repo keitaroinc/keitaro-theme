12231c9dea61b117fbf5a08d3159bec3d66d2d61
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-22 16:33:52 +0200

    Add conditional blocks for avatars and post sidebar

--- template-snippets/content/content.php
+++ template-snippets/content/content.php
@@ -1,6 +1,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     <div class="row">
+        <?php if (!is_author() && !is_single()): ?>
+            <div class="col-md-2 avatar-wrapper">
+                <?php keitaro_author_avatar(get_the_author_meta('ID')); ?>
+            </div>
+        <?php endif; ?>
         <div class="col-md-8">
             <?php
 
@@ -9,31 +14,30 @@
             if (is_archive() || is_home() || is_search()) :
                 get_template_part(SNIPPETS_DIR . '/entry-excerpt');
             else :
-                if ('' !== get_the_post_thumbnail()) :
-                    get_template_part(SNIPPETS_DIR . '/post-thumbnail');
-                endif;
                 get_template_part(SNIPPETS_DIR . '/entry-content');
             endif;
 
             comments_template();
 
             ?>
-        </div>
-        <div class="col-md-4">
-            <?php
+        </div> 
+        <?php if (is_single()): ?>
+            <div class="col-md-4">
+                <?php
 
-            if (!is_search()):
-                keitaro_child_pages_list(get_the_ID());
+                if (!is_search()):
+                    keitaro_child_pages_list(get_the_ID());
 
-                foreach (get_children(get_ancestors(get_the_ID())) as $page) :
-                    get_template_part(SNIPPETS_DIR . '/sidebars/icon-blocks');
-                endforeach;
-            endif;
+                    foreach (get_children(get_ancestors(get_the_ID())) as $page) :
+                        get_template_part(SNIPPETS_DIR . '/sidebars/icon-blocks');
+                    endforeach;
+                endif;
 
-            get_template_part(SNIPPETS_DIR . '/entry-footer');
+                get_template_part(SNIPPETS_DIR . '/entry-footer');
 
-            ?>
-        </div>
+                ?>
+            </div>
+        <?php endif; ?>
     </div>
 
 </article>
