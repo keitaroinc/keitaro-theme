cc73508267a27dd875481ec88786acad556344de
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-22 16:28:07 +0200

    Add commented block for discovering HTML elements which cause overflow

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -47,6 +47,19 @@
             btnGoToTop.removeClass(btnGoToTopToggleClass);
         }
 
+
+
     });
+    
+//    var docWidth = document.documentElement.offsetWidth;
+//
+//    [].forEach.call(
+//            document.querySelectorAll('*'),
+//            function (el) {
+//                if (el.offsetWidth > docWidth) {
+//                    console.log(el);
+//                }
+//            }
+//    );
 
 })(jQuery);
