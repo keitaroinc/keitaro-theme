00b981f2655fad8a68f6aee4064cc301fdd3627a
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-22 16:30:39 +0200

    Add small title specifically for Sticky posts and restructure if-else blocks

--- template-snippets/header/entry-header.php
+++ template-snippets/header/entry-header.php
@@ -1,21 +1,27 @@
 <header class="entry-header">
     <?php
 
-    if (is_singular()) {
-        the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-    } elseif (is_front_page() && is_home()) {
-        the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h3>');
-    } else {
-        the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
-    }
+    if (is_sticky() && !is_single()):
+        printf('<p class="sticky-title-sm">%s</p>', __('Must read', 'keitaro'));
+    endif;
 
-    if ('post' === get_post_type()) {
+    if (is_singular()) :
+        the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
+    elseif (is_front_page() && is_home()) :
+        the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h3>');
+    elseif (is_sticky()) :
+        the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+    else :
+        the_title('<h2 class="entry-title h3"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+    endif;
+
+    if ('post' === get_post_type()) :
         echo '<div class="entry-meta">';
-        if (is_single() || is_archive() || is_home()) {
+        if (is_single() || is_archive() || is_home()) :
             keitaro_posted_on();
-        }
+        endif;
         echo '</div><!-- .entry-meta -->';
-    };
+    endif;
 
     ?>
 </header>
