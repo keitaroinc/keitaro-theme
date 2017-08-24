043f972f5ef1ca1090a64600f6146a370d268b3a
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-24 16:34:07 +0200

    Add string-escaping functions

--- comments.php
+++ comments.php
@@ -18,11 +18,11 @@ if ( post_password_required() ) {
 			$comments_number = get_comments_number();
 			if ( '1' === $comments_number ) {
 				/* translators: %s: post title */
-				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'keitaro' ), get_the_title() );
+				printf( esc_html( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'keitaro' ) ), get_the_title() );
 			} else {
 				printf(
 					/* translators: 1: number of comments, 2: post title */
-					_nx(
+					esc_html( _nx(
 						'%1$s Reply to &ldquo;%2$s&rdquo;',
 						'%1$s Replies to &ldquo;%2$s&rdquo;',
 						$comments_number,
@@ -31,7 +31,8 @@ if ( post_password_required() ) {
 					),
 					number_format_i18n( $comments_number ),
 					get_the_title()
-				);
+                    )
+                );
 			}
 			?>
 		</h3>
@@ -55,7 +56,7 @@ if ( post_password_required() ) {
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
 	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'keitaro' ); ?></p>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'keitaro' ); ?></p>
 	<?php
 	endif;
 
