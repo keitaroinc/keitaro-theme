9372390fac308d920bf5c75d628b798dee8be328
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-24 13:38:29 +0200

    Add content-read-next.php snippet

---
+++ template-snippets/content/content-read-next.php
@@ -0,0 +1,54 @@
+<?php
+
+$post_cats = '';
+$post_tags = '';
+
+// Get current post tags
+foreach (get_the_terms(get_the_ID(), 'post_tag') as $tag):
+    $post_tags[] = $tag->term_id;
+endforeach;
+
+// Get current post categories
+foreach (get_the_terms(get_the_ID(), 'category') as $tag):
+    $post_cats[] = $tag->term_id;
+endforeach;
+
+// Get posts that have any of the tags and categories of the current post
+$read_more_content = new WP_Query(
+        array(
+    'post__not_in' => array(get_the_ID()),
+    'posts_per_page' => 3,
+    'tags__in' => $post_tags,
+    'category__in' => $post_cats
+        )
+);
+
+?>
+<section class="read-next">
+    <h4 class="read-next-title"><?php _e('Read next', 'keitaro'); ?></h4>
+    <?php
+
+    if ($read_more_content->have_posts()):
+
+        while ($read_more_content->have_posts()):
+
+            $read_more_content->the_post();
+
+            ?>
+            <div class="media">
+                <div class="media-left">
+                    <?php keitaro_author_avatar(get_the_author_meta('ID'), 48); ?>
+                </div>
+                <div class="media-body">
+                    <?php the_title('<h4 class="media-heading"><a href="' . get_permalink() . '">', '</a></h4>'); ?>
+                </div>
+            </div>
+
+            <?php
+
+        endwhile;
+
+    endif;
+
+    ?>
+</section>
