6862271fb1eea1acbfd31757bda1fd57a2d8fda8
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-25 17:40:04 +0200

    Remove comments_template() from page.html

--- template-snippets/content/content-page.php
+++ template-snippets/content/content-page.php
@@ -1,27 +1,26 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="row">
-		<div class="col-md-8">
-			<?php
+    <div class="row">
+        <div class="col-md-8">
+            <?php
 
-			get_template_part( SNIPPETS_DIR . '/header/entry-header' );
-			get_template_part( SNIPPETS_DIR . '/entry-content' );
+            get_template_part( SNIPPETS_DIR . '/header/entry-header' );
 
-			comments_template();
+            get_template_part( SNIPPETS_DIR . '/entry-content' );
 
-			?>
-		</div>
-		<div class="col-md-4">
-			<?php
+            ?>
+        </div>
+        <div class="col-md-4">
+            <?php
 
-			if ( '' !== get_the_post_thumbnail() && ! is_single() ) :
-				get_template_part( SNIPPETS_DIR . '/post-thumbnail' );
-			endif;
+            if ( '' !== get_the_post_thumbnail() && !is_single() ) :
+                get_template_part( SNIPPETS_DIR . '/post-thumbnail' );
+            endif;
 
-			keitaro_child_pages_list( get_the_ID() );
+            keitaro_child_pages_list( get_the_ID() );
 
-			get_template_part( SNIPPETS_DIR . '/sidebars/icon-blocks' );
+            get_template_part( SNIPPETS_DIR . '/sidebars/icon-blocks' );
 
-			?>
-		</div>
-	</div>
+            ?>
+        </div>
+    </div>
 </article>
