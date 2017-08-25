a86c72f4f5011e249fecb46311aa62abd76b0e72
Goce Mitevski <goce.mitevski@keitaro.com>
2017-08-25 17:35:14 +0200

    Add thumbnail caption and restructure code

--- template-snippets/post-thumbnail.php
+++ template-snippets/post-thumbnail.php
@@ -6,9 +6,12 @@ else :
 	$thumbnail_size = 'thumbnail';
 endif;
 
+$thumbnail_caption = esc_html( get_the_post_thumbnail_caption() );
+
 ?>
-<div class="post-thumbnail">
-	<a href="<?php the_permalink(); ?>">
+<figure class="post-thumbnail">
+    <a href="<?php (is_single() ? the_post_thumbnail_url() : the_permalink() ); ?>">
 		<?php the_post_thumbnail( $thumbnail_size ); ?>
 	</a>
-</div>
+    <?php if ($thumbnail_caption) : printf( '<figcaption>%s</figcaption>', $thumbnail_caption ); endif ?>
+</figure>
