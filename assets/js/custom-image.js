f13539d6685a215b985fcbea8e862602ba95880d
Goce Mitevski <goce.mitevski@keitaro.com>
2017-09-05 16:47:32 +0200

    Fix Icon Block widget image not being stored on Save

--- assets/js/custom-image.js
+++ assets/js/custom-image.js
@@ -3,24 +3,18 @@
  */
 (function ($) {
     $(document).ready(function ($) {
-        var customImageBtnAdd = $('.custom-image');
-        var customImageBtnRemove = $('.custom-image-remove');
 
         /* Uploading files */
         var file_frame;
 
-
-// KONTROLIRAJ GI FUNKCIONALNOSTITE PO PARENT, NE ZA SITE ELEMENTI
-// MOZNO E DA TREBA DA SETIRASH I ID-A
-
-        customImageBtnRemove.on('click', function (event) {
-            customImageBtnRemove$('.custom-image-value').val('');
-            customImageBtnRemove.parent().parent().closest('input[name*="savewidget"]')[0].click();
+        $('.custom-image-remove').on('click', function (event) {
+            $(this).parent().parent().find('.custom-image-value').val('');
         });
 
-        customImageBtnAdd.on('click', function (event) {
+        $('.custom-image').on('click', function (event) {
 
             event.preventDefault();
+            var parentForm = $(this).parent().parent();
 
             // If the media frame already exists, reopen it.
             if (file_frame) {
@@ -43,9 +37,8 @@
                 attachment = file_frame.state().get('selection').first().toJSON();
 
                 // Do something with attachment.id and/or attachment.url here
-                customImageBtnAdd.parent().find('.current-custom-image').attr('src', attachment.url);
-                customImageBtnAdd.parent().find('.custom-image-value').val(attachment.id);
-                customImageBtnAdd.parent().parent().closest('input[name*="savewidget"]')[0].click();
+                parentForm.find('.current-custom-image').attr('src', attachment.url);
+                parentForm.find('.custom-image-value').val(attachment.id);
             });
 
             //If the uploader object has already been created, reopen the dialog
