a9f06e662bb10d6e0f9d579967eacdb7bd6e519b
Goce Mitevski <goce.mitevski@keitaro.com>
2017-09-27 14:49:19 +0200

    Add missing pagination on Author template

--- index.php
+++ index.php
@@ -50,6 +50,8 @@ if ( ! is_front_page() ) :
 
 				<?php
 
+                        else:
+                                get_template_part( SNIPPETS_DIR . '/navigation/pagination' );
 			endif;
 
 			get_template_part( SNIPPETS_DIR . '/sidebars/twitter-content' );
