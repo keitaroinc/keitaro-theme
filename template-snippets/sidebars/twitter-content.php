1742c7e6ae37e7883609ad535fe6f0f6cf34eb75
Goce Mitevski <goce.mitevski@keitaro.com>
2018-01-11 14:03:23 +0100

    Add .container-fluid wrapper

--- template-snippets/sidebars/twitter-content.php
+++ template-snippets/sidebars/twitter-content.php
@@ -1,7 +1,9 @@
 <?php if ( is_home() && $paged < 1 ) : ?>
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<?php dynamic_sidebar( 'keitaro_twitter' ); ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<?php dynamic_sidebar( 'keitaro_twitter' ); ?>
+			</div>
 		</div>
 	</div>
 	<?php
