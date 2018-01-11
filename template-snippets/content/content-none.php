165b2474a74549118066e9a29d343bd8f63e6e20
Goce Mitevski <goce.mitevski@keitaro.com>
2018-01-11 13:54:02 +0100

    Separate .container wrapper from .content-area

--- template-snippets/content/content-none.php
+++ template-snippets/content/content-none.php
@@ -1,36 +1,38 @@
-<div class="container content-area">
-    <section class="no-results not-found">
-        <header class="page-header">
-            <h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
-		</header>
-		<div class="page-content">
-			<?php
-
-			if ( is_home() && current_user_can( 'publish_posts' ) ) :
-
-				?>
-
-				<p>
-					<?php
-
-					// translators: Shown when no posts have been published yet and linked to the page for creating a new post
-					echo wp_kses_post( sprintf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'keitaro' ), esc_url( admin_url( 'post-new.php' ) ) ) );
-
-					?>
-				</p>
-
-			<?php else : ?>
-
-				<p><?php esc_html_e( "It seems we can't find what you&rsquo;re looking for. Perhaps searching can help.", 'keitaro' ); ?></p>
+<div class="container">
+    <div class="content-area">
+        <section class="no-results not-found">
+            <header class="page-header">
+                <h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
+			</header>
+			<div class="page-content">
 				<?php
 
-				if ( ! is_search() ) :
-					get_search_form();
+				if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+					?>
+
+					<p>
+						<?php
+
+						// translators: Shown when no posts have been published yet and linked to the page for creating a new post
+						echo wp_kses_post( sprintf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'keitaro' ), esc_url( admin_url( 'post-new.php' ) ) ) );
+
+						?>
+					</p>
+
+				<?php else : ?>
+
+					<p><?php esc_html_e( "It seems we can't find what you&rsquo;re looking for. Perhaps searching can help.", 'keitaro' ); ?></p>
+					<?php
+
+					if ( ! is_search() ) :
+						get_search_form();
+					endif;
+
 				endif;
 
-			endif;
-
-			?>
-		</div><!-- .page-content -->
-	</section><!-- .no-results -->
+				?>
+			</div><!-- .page-content -->
+		</section><!-- .no-results -->
+	</div><!-- .content-area -->
 </div><!-- .container -->
