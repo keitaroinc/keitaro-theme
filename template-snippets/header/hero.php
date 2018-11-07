30d9cd2ee70f276f84b0e82672729886fd20563f
Goce Mitevski <goce.mitevski@keitaro.com>
2018-11-07 15:05:20 +0100

    Refactor hero.php snippet

--- template-snippets/header/hero.php
+++ template-snippets/header/hero.php
@@ -12,7 +12,7 @@ $hero_class   = '';
 $header_image = get_header_image();
 
 if ( DEFAULT_HEADER_IMAGE === $header_image ) :
-	$header_image_style = 'style="background-image: url(' . esc_attr( $header_image ) . '), url(' . DEFAULT_HEADER_IMAGE_EXTEND . ')"';
+	$header_image_style = 'style="background-image: url(' . esc_attr( $header_image ) . ')"';
 else :
 	$hero_class         = 'hero-non-default';
 	$header_image_style = 'style="background-image: url(' . esc_attr( $header_image ) . '); background-size: cover"';
@@ -20,12 +20,8 @@ endif;
 
 ?>
 <div class="hero <?php echo esc_attr( $hero_class ); ?>" <?php echo ( esc_url( $header_image ) ? wp_kses( $header_image_style, array( 'style' ) ) : '' ); ?>>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-lg-7">
-				<?php do_shortcode( '[keitaro-hero-title]' ); ?>
-			</div>
-		</div>
-		<?php get_template_part( SNIPPETS_DIR . '/sidebars/services' ); ?>
+	<div class="hero-wrapper">
+		<?php do_shortcode( '[keitaro-hero-title]' ); ?>
 	</div>
 </div>
+<?php get_template_part( SNIPPETS_DIR . '/sidebars/services' );
