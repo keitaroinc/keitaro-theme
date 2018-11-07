cdc07ab18699c6793fe796dda9298970477df342
Goce Mitevski <goce.mitevski@keitaro.com>
2018-11-07 15:06:38 +0100

    Refactor header.php template

--- header.php
+++ header.php
@@ -21,32 +21,29 @@
 	<body <?php body_class(); ?>>
 		<?php
 
-		if ( is_front_page() ) :
-			$container_class = 'container';
-		else :
-			$container_class = 'container-fluid';
-		endif;
+		$container_wrapper_class = 'container-fluid';
+		$container_bg_class = '';
 
-		if ( get_header_image() === DEFAULT_HEADER_IMAGE ) :
-			$container_wrapper_class = 'container-bottom-border';
-		else :
-			$container_wrapper_class = '';
+		if ( ! is_front_page() ) :
+			$container_bg_class = 'container-bg';
 		endif;
 
 		?>
-		<div class="container-bg <?php echo esc_html( $container_wrapper_class ); ?>">
-			<div class="<?php echo esc_html( $container_class ); ?>">
-				<header class="main-navbar content-block">
+		<div class="<?php echo esc_html( $container_bg_class ); ?> <?php echo esc_html( $container_wrapper_class ); ?>">
+		<div class="row">
+			<div class="col-lg-10 col-lg-offset-1">
+					<header class="main-navbar content-block">
 						<?php get_template_part( SNIPPETS_DIR . '/header/logo' ); ?>
 						<?php get_template_part( SNIPPETS_DIR . '/navigation/main-menu' ); ?>
-				</header>
+					</header>
+				</div>
 			</div>
 		</div>
 
 		<?php if ( ! is_front_page() ) : ?>
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-md-8 col-lg-9">
+				<div class="col-md-8 col-lg-7 col-lg-offset-1">
 					<?php get_template_part( SNIPPETS_DIR . '/navigation/breadcrumb' ); ?>
 				</div>
 				<?php if ( have_posts() && ! is_page() ) : ?>
@@ -54,7 +51,7 @@
 						<?php get_search_form(); ?>
 					</div>
 				<?php endif; ?>
-			</div> 
+			</div>
 		</div>
 		<?php
 		endif;
