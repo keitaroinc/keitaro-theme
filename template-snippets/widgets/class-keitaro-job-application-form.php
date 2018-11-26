ccedd2e57679dc4437716430aea515d857379200
Goce Mitevski <goce.mitevski@keitaro.com>
2018-11-26 10:35:31 +0100

    Re-add

--- template-snippets/widgets/class-keitaro-job-application-form.php
+++ template-snippets/widgets/class-keitaro-job-application-form.php
@@ -75,7 +75,9 @@ class Keitaro_Job_Application_Form extends WP_Widget {
 
 					$uploadedfile = $_FILES['upload'];
 
-					$movefile = wp_handle_upload( $uploadedfile );
+					$upload_overrides = array( 'test_form' => false );
+
+					$movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
 
 					if ( ! $movefile && isset( $movefile['error'] ) ) {
 						/**
