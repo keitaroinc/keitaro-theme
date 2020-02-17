42f908b82d409972f7edb22f02c1932ac8de6fe3
Goce Mitevski <goce.mitevski@keitaro.com>
2020-02-17 22:46:24 +0100

    Replace offset CSS classes

--- template-snippets/sidebars/services.php
+++ template-snippets/sidebars/services.php
@@ -15,7 +15,7 @@ if ( is_active_sidebar( $sidebar_id ) ) :
 	?>
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-lg-10 col-lg-offset-1">
+		<div class="col-lg-10 offset-md-1">
 			<div class="services">
 				<?php dynamic_sidebar( $sidebar_id ); ?>
 			</div>
