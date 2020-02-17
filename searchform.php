b71da5e4d2677f6d5a8aab4e40ce317f9dc00f60
Goce Mitevski <goce.mitevski@keitaro.com>
2020-02-17 22:44:27 +0100

    Replace btn-default with btn-outline-secondary

--- searchform.php
+++ searchform.php
@@ -16,7 +16,7 @@ $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 	<div class="input-group">
 		<input type="search" id="<?php echo esc_attr( $unique_id ); ?>" class="form-control search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'keitaro' ); ?>" required="required" value="<?php echo get_search_query(); ?>" name="s" />
 		<span class="input-group-btn">
-			<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+			<button type="submit" class="btn btn-outline-secondary"><span class="glyphicon glyphicon-search"></span></button>
 		</span>
 	</div>
 
