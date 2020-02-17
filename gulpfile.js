6ed1f1c361a12368d9995b7068f6824a118af7df
Goce Mitevski <goce.mitevski@keitaro.com>
2020-02-17 21:39:59 +0100

    Refactor gulpfile.js tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
-var less = require('gulp-less');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
 var cleanCSS = require('gulp-clean-css');
 //var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
@@ -7,18 +10,20 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var imagemin = require('gulp-imagemin');
 
+var jsAssets = ['assets/js/*.js', '!assets/js/*.min.js'];
+
 // Rebuild CSS from LESS
-gulp.task('less', function () {
-	return gulp.src('assets/less/**/style.less')
-		//            .pipe(sourcemaps.init())
-		.pipe(less())
+gulp.task('sass', function () {
+	return gulp.src('assets/scss/**/style.scss')
+		// .pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: 'last 3 versions'
 		}))
 		.pipe(cleanCSS({
 			compatibility: 'ie8'
 		}))
-		// .pipe(sourcemaps.write()) - Uncoment when developing
+		// .pipe(sourcemaps.write()) - Uncomment when developing
 		.pipe(gulp.dest('.'));
 });
 
@@ -49,7 +54,7 @@ gulp.task('bootstrap-js', function () {
 // Copy Bootstrap font files in assets/fonts
 gulp.task('bootstrap-fonts', function () {
 	return gulp.src('node_modules/bootstrap/dist/fonts/*')
-		.pipe(gulp.dest('assets/fonts/'));
+		.pipe(gulp.dest('assets/fonts'));
 });
 
 // jQuery assets
@@ -60,10 +65,10 @@ gulp.task('jquery', function () {
 
 // JS minify
 gulp.task('js', function () {
-	return gulp.src(['!assets/js/*.min.js', 'assets/js/*.js'])
+	return gulp.src(jsAssets)
 		.pipe(uglify())
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('assets/js/'));
+		.pipe(gulp.dest('assets/js'));
 });
 
 // Minify images
@@ -73,7 +78,7 @@ gulp.task('img', function () {
 			imagemin.optipng({ optimizationLevel: 7 }),
 			imagemin.svgo({
 				plugins: [
-					{ removeViewBox: true }				]
+					{ removeViewBox: true }]
 			})
 		]))
 		.pipe(gulp.dest('assets/img/'));
@@ -81,9 +86,9 @@ gulp.task('img', function () {
 
 // Watch for LESS and JS file changes
 gulp.task('watch', function () {
-	gulp.watch(['assets/less/**/*.less'], gulp.parallel('less'));
-	gulp.watch(['assets/js/**/*.js'], gulp.parallel('js'));
+	gulp.watch(['assets/scss/**/*.scss'], gulp.parallel('sass'));
+	gulp.watch(jsAssets, gulp.parallel('js'));
 });
 
 // The default Gulp.js task
-gulp.task('default', gulp.series('bootstrap-fonts', 'jquery', 'bootstrap-js', 'clipboard', 'prism-js', 'prism-css', 'img', 'js', 'less', 'watch'));
+gulp.task('default', gulp.parallel('bootstrap-fonts', 'jquery', 'bootstrap-js', 'clipboard', 'prism-js', 'prism-css', 'img', 'js', 'sass', 'watch'));
