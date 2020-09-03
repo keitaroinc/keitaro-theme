9e1de34387a4fa14489995ec66c9acffc95e3c5f
Nikola Lekich <you@example.com>
2020-09-03 10:13:13 +0200

    Removed the service icons displayed on the homepage.

--- sidebar.php
+++ sidebar.php
@@ -9,9 +9,9 @@
  */
 
 // Show Service Icons widget section on the front page
-if ( is_front_page() ) :
-	get_template_part( SNIPPETS_DIR . '/sidebars/service-icons' );
-endif;
+// if ( is_front_page() ) :
+// 	get_template_part( SNIPPETS_DIR . '/sidebars/service-icons' );
+// endif;
 
 // Show Call to Action widget section on all static pages or the front page
 // if ( is_front_page() || is_page() ) :
