a1a3c17c4f2c4fa15e926560b42b73fafcf0832c
Nikola Lekich <you@example.com>
2020-09-08 10:38:05 +0200

    Improvements on small screen for the custom blocks.

--- template-snippets/sidebars/core-team.php
+++ template-snippets/sidebars/core-team.php
@@ -12,7 +12,7 @@ $sidebar_id = 'keitaro_core_team';
 if ( is_active_sidebar( $sidebar_id ) ) :
 	?>
 <div class="core-section text-center">
-	<h1 class="mb-0 mt-5">Core Team</h1>
+	<h3 class="mb-0 mt-5">CORE TEAM</h3>
 	<div class="container px-lg-0 px-5 ">
 		<div class="row justify-content-center pt-5 mt-5">
 			<?php dynamic_sidebar( $sidebar_id ); ?>
