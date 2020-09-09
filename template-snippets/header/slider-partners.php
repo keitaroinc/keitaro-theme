0b9933fb86a33b4de7aaff2b301e0f35c9285ff9
Nikola Lekich <you@example.com>
2020-09-10 00:45:19 +0200

    added new logos for the hero page slider

--- template-snippets/header/slider-partners.php
+++ template-snippets/header/slider-partners.php
@@ -9,55 +9,182 @@
  */
 ?>
 
-<div class="partners-carousel bg-white">
-<div class="container" >
+
+
+	<div class="partners-carousel-small bg-white">
+<div class="container-fluid" >
 	<p >Trusted by</p>
 			<div id="carouselExampleControls" class="carousel slide bg-white"  data-ride="carousel">
+
+			<div class="controls-top">
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="" data-slide="prev"><i class="fa fa-chevron-left"></i></a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="" data-slide="next"><i class="fa fa-chevron-right"></i></a>
+			</div>
 			<div class="carousel-inner">
 
+			
 				<div class="carousel-item active">
 					<div class="row bg-white d-flex flex-wrap align-items-center">
 						<div class="col-3 ">
-						<img class="d-block img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-1.png" alt="First slide">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//CFA.png">
 						</div>
 						<div class="col-3">
-						<img class="d-block  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-2.png" alt="Second slide">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//CFE.png">
 						</div>
 						<div class="col-3">
-						<img class="d-block  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-3.png" alt="third slide">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Group 7.png">
 						</div>
 						<div class="col-3">
-						<img class="d-block  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-4.png" alt="forth slide">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Group 8.png">
 						</div>
 					</div>
 				</div>
 
 				<div class="carousel-item">
-				<div class="row bg-white d-flex flex-wrap align-items-center">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
 						<div class="col-3">
-						<img class="d-block img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-1.png" alt="First slide">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Health-africa2.png">
 						</div>
 						<div class="col-3">
-						<img class="d-block img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-2.png" alt="Second slide">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//OKF.png">
 						</div>
 						<div class="col-3">
-						<img class="d-block img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-3.png" alt="third slide">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Quench-tech.png">
 						</div>
 						<div class="col-3">
-						<img class="d-block img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//parntens-logo-4.png" alt="forth slide">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Toronto.png">
+						</div>
+					</div>
+				</div>
+
+				<div class="carousel-item">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//UNHCR.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//apis2.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//byteyko.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//energinet.png">
+						</div>
+					</div>
+				</div>
+
+				<div class="carousel-item">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//fossa2.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//iBasis.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//maze.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//shell.png">
 						</div>
 					</div>
 				</div>
 
 			</div>
-			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
+		
 		</div>
 </div>
 </div>
+
+<!-- <div class="partners-carousel-large bg-white">
+<div class="container-fluid" >
+	<p >Trusted by</p>
+			<div id="carouselExampleControls" class="carousel slide bg-white"  data-ride="carousel">
+
+			<div class="controls-top">
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="" data-slide="prev"><i class="fa fa-chevron-left"></i></a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="" data-slide="next"><i class="fa fa-chevron-right"></i></a>
+			</div>
+			<div class="carousel-inner">
+
+			
+				<div class="carousel-item active">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
+						<div class="col-2 ">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//CFA.png">
+						</div>
+						<div class="col-2">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//CFE.png">
+						</div>
+						<div class="col-2">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Group 7.png">
+						</div>
+						<div class="col-2">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Group 8.png">
+						</div>
+						<div class="col-2">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Health-africa2.png">
+						</div>
+						<div class="col-2">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//OKF.png">
+						</div>
+					</div>
+				</div>
+
+				<div class="carousel-item">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
+						<div class="col-3">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Health-africa2.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//OKF.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Quench-tech.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto  img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//Toronto.png">
+						</div>
+					</div>
+				</div>
+
+				<div class="carousel-item">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//UNHCR.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//apis2.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//byteyko.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//energinet.png">
+						</div>
+					</div>
+				</div>
+
+				<div class="carousel-item">
+					<div class="row bg-white d-flex flex-wrap align-items-center">
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//fossa2.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//iBasis.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//maze.png">
+						</div>
+						<div class="col-3">
+						<img class="d-block m-auto img-fluid" src="<?php echo  get_theme_file_uri(); ?>/assets//img//partners-img//shell.png">
+						</div>
+					</div>
+				</div>
+
+			</div>
+		
+		</div>
+</div>
+</div> -->
