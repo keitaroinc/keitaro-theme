df40427489c54d7410df57b8ce927eb39ae014af
Nikola Lekich <you@example.com>
2020-09-15 14:11:34 +0200

    Implemented the design for the information cards on the single page.

---
+++ single.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Single
+ *
+ * @link https://github.com/keitaroinc/keitaro-theme
+ *
+ * @package WordPress
+ * @subpackage Keitaro
+ *
+ */
+
+get_header();
+?>
+<div class="single-page-content">
+  <?php the_content(); ?>
+</div>
+<?php
+get_footer();
