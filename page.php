079e8cddf76ee4fa941ad0fc4fb7ea7b8be1f747
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-17 13:11:38 +0200

    Add page.php template

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template for all pages, when not overridden
+ *
+ * @link https://github.com/keitaroinc/keitaro-theme
+ *
+ * @package WordPress
+ * @subpackage Keitaro
+ */
+
+get_header();
+
+the_content();
+
+get_footer();
