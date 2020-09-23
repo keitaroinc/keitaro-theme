a831d9b4458f34bce099d804283508db8a63334e
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-23 17:26:22 +0200

    Modify entry-header.php content

--- template-snippets/header/entry-header.php
+++ template-snippets/header/entry-header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template snippet for the header of each post
  *
@@ -12,26 +13,16 @@
 <header class="entry-header">
 	<?php
 
-	if ( is_sticky() && ! is_single() ) :
-		printf( '<p class="sticky-title-sm">%s</p>', esc_html__( 'Must read', 'keitaro' ) );
-	endif;
+	the_category();
 
-	if ( is_singular() ) :
-		the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-	elseif ( is_front_page() && is_home() ) :
-		the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-	elseif ( is_sticky() ) :
-		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+	if (is_singular()) :
+		the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
+	elseif (is_front_page() && is_home()) :
+		the_title('<h3 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h3>');
+	elseif (is_sticky()) :
+		the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
 	else :
-		the_title( '<h2 class="entry-title h3"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-	endif;
-
-	if ( 'post' === get_post_type() ) :
-		echo '<div class="entry-meta">';
-		if ( is_singular() || is_archive() || is_home() || is_search() ) :
-			keitaro_posted_on();
-		endif;
-		echo '</div><!-- .entry-meta -->';
+		the_title('<h2 class="entry-title h3"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
 	endif;
 
 	?>
