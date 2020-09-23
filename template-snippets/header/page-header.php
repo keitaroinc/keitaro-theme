4acbb5c7a2783ca91449dae38c73d3239e506253
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-23 17:24:21 +0200

    Refactor page-header.php

--- template-snippets/header/page-header.php
+++ template-snippets/header/page-header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template snippet for .page-header
  *
@@ -11,41 +12,47 @@
 $page_title    = '';
 $page_subtitle = '';
 
-if ( is_author() ) :
-	$page_title = __( 'Content by:', 'keitaro' );
+if (is_author()) :
+	$page_title = __('Content by:', 'keitaro');
 
-elseif ( is_search() ) :
+elseif (is_search()) :
 	global $wp_query;
-	$page_title    = __( 'Search results:', 'keitaro' ) . ' ' . highlight( get_search_query() );
-	$page_subtitle = __( 'Found', 'keitaro' ) . ' ' . highlight( $wp_query->found_posts ) . ' ' . __( 'search results', 'keitaro' );
+	$page_title    = __('Search results:', 'keitaro') . ' ' . highlight(get_search_query());
+	$page_subtitle = __('Found', 'keitaro') . ' ' . highlight($wp_query->found_posts) . ' ' . __('search results', 'keitaro');
 
-elseif ( is_archive() ) :
+elseif (is_archive()) :
 	$page_title = get_the_archive_title();
+	$page_subtitle = get_the_archive_description();
 
+elseif (is_home()) :
+	$page_title = single_post_title('', false);
 endif;
 
-if ( ! empty( $page_title ) ) :
+if (!empty($page_title)) :
 
-	?>
+?>
 	<header class="page-header">
 		<div class="row">
-			<div class="col-md-8">
-				<?php if ( $page_title ) : ?>
-							<h1 class="page-title"><?php echo wp_kses_post( $page_title ); ?></h1>
-				<?php endif; ?>
-				<?php if ( $page_subtitle ) : ?>
-					<p class="lead"><?php echo wp_kses_post( $page_subtitle ); ?></p>
+			<div class="col-md-8 offset-md-2">
+				<?php if ($page_title) : ?>
+				<div class="d-flex align-items-center justify-content-center">
+					<h1 class="page-title"><?php echo wp_kses_post($page_title); ?></h1>
 					<?php
+					if (is_author()) :
+						keitaro_author_box(get_the_author_meta('ID'));
+					endif;
+					?>
+				</div>
+				<?php endif; ?>
+				<?php if ($page_subtitle) : ?>
+					<div class="lead"><?php echo wp_kses_post($page_subtitle); ?></div>
+				<?php
 
-				endif;
-
-if ( is_author() ) :
-	keitaro_author_box( get_the_author_meta( 'ID' ) );
 				endif;
 
 				?>
 			</div>
 		</div>
 	</header>
-	<?php
+<?php
 endif;
