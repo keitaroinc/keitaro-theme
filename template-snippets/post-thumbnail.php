92634f107defbe98bffef3aafd01ffd9531829ce
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-23 17:23:00 +0200

    Refactor post-thumbnail.php

--- template-snippets/post-thumbnail.php
+++ template-snippets/post-thumbnail.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template snippet for the thumbnail of each post
  *
@@ -8,23 +9,23 @@
  * @subpackage Keitaro
  */
 
-if ( is_single() ) :
-	$thumbnail_size = 'large';
+if (is_single()) :
+	$thumbnail_size = 'full';
 else :
-	$thumbnail_size = 'thumbnail';
+	$thumbnail_size = 'large';
 endif;
 
-$thumbnail_caption = esc_html( get_the_post_thumbnail_caption() );
+$thumbnail_caption = esc_html(get_the_post_thumbnail_caption());
 
 ?>
 <figure class="post-thumbnail">
-	<a href="<?php ( is_single() ? the_post_thumbnail_url() : the_permalink() ); ?>">
-		<?php the_post_thumbnail( $thumbnail_size ); ?>
+	<a class="d-block" href="<?php (is_attachment() ? the_post_thumbnail_url() : the_permalink()); ?>">
+		<?php the_post_thumbnail($thumbnail_size); ?>
 	</a>
 	<?php
 
-	if ( $thumbnail_caption ) :
-		printf( '<figcaption>%s</figcaption>', wp_kses_post( $thumbnail_caption ) );
+	if ($thumbnail_caption) :
+		printf('<figcaption>%s</figcaption>', wp_kses_post($thumbnail_caption));
 	endif
 
 	?>
