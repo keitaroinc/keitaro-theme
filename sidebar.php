90785fa90d897d5e6b01227343f49868e3408d5d
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-23 17:20:12 +0200

    Reset default Sidebar template

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template for loading sidebar content
  *
@@ -8,18 +9,4 @@
  * @subpackage Keitaro
  */
 
-// Show Service Icons widget section on the front page
-// if ( is_front_page() ) :
-// 	get_template_part( SNIPPETS_DIR . '/sidebars/service-icons' );
-// endif;
-
-// Show Call to Action widget section on all static pages or the front page
-// if ( is_front_page() || is_page() ) :
-// 	get_template_part( SNIPPETS_DIR . '/sidebars/call-to-action' );
-// endif;
-
-// Show  the latest four posts on the frontpage
-if ( is_front_page() ) :
-get_template_part( SNIPPETS_DIR . '/frontpage-content' );
-endif;
-
+dynamic_sidebar();
