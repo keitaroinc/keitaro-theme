db55fdc026392d8fd992e5efd8bab08402fa1d70
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-24 10:04:54 +0200

    Add gutenberg-overrides.js and its minified version

---
+++ assets/js/gutenberg-overrides.js
@@ -0,0 +1,31 @@
+// Custom overrides to various Gutenberg components
+
+wp.blocks.registerBlockStyle('core/columns', {
+	name: 'service-group',
+	label: 'Service Group'
+});
+
+wp.blocks.registerBlockStyle('core/columns', {
+	name: 'team-group',
+	label: 'Team Group'
+});
+
+wp.blocks.registerBlockStyle('core/media-text', {
+	name: 'media-text-feature',
+	label: 'Feature'
+});
+
+wp.blocks.registerBlockStyle('core/media-text', {
+	name: 'media-text-feature-transparent',
+	label: 'Feature with Transparent Image'
+});
+
+wp.blocks.registerBlockStyle('core/media-text', {
+	name: 'media-text-partner',
+	label: 'Partner'
+});
+
+wp.blocks.registerBlockStyle('core/latest-posts', {
+	name: 'latest-posts-insights',
+	label: 'Insights'
+});
