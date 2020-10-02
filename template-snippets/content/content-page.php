346a63153d57ff55bcc1fe0f1bdf2062295b6aa0
Goce Mitevski <goce.mitevski@keitaro.com>
2020-10-02 14:26:55 +0200

    Update content-page.php template snippet

--- template-snippets/content/content-page.php
+++ template-snippets/content/content-page.php
@@ -10,31 +10,16 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="">
-		<div class="col-lg-12">
-			<?php
+<article id="post-<?php the_ID(); ?>" <?php post_class('my-5'); ?>>
+	<?php
 
-			get_template_part( SNIPPETS_DIR . '/header/entry-header' );
+	get_template_part( SNIPPETS_DIR . '/header/entry-header' );
 
-			get_template_part( SNIPPETS_DIR . '/entry-content' );
+	if ( '' !== get_the_post_thumbnail() && ! is_single() ) :
+		get_template_part( SNIPPETS_DIR . '/post-thumbnail' );
+	endif;
 
-			?>
-		</div>
-		<div class="col-lg-4">
-			<?php
+	get_template_part( SNIPPETS_DIR . '/entry-content' );
 
-			if ( '' !== get_the_post_thumbnail() && ! is_single() ) :
-				get_template_part( SNIPPETS_DIR . '/post-thumbnail' );
-			endif;
-
-			get_template_part( SNIPPETS_DIR . '/sidebars/page-widgets' );
-
-			keitaro_child_pages_list( get_the_ID() );
-
-			get_template_part( SNIPPETS_DIR . '/sidebars/icon-blocks' );
-
-			?>
-		</div>
-	</div>
+	?>
 </article>
