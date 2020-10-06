8970dcff707db7e35852ffe65626a9365ef33190
Goce Mitevski <goce.mitevski@keitaro.com>
2020-10-06 21:53:37 +0200

    Update content-first and content-grid, post-author rendering logic

--- template-snippets/content/content-first.php
+++ template-snippets/content/content-first.php
@@ -10,18 +10,20 @@
  */
 
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'hentry-first' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('hentry-first'); ?>>
 	<?php
 
-	if ( has_post_thumbnail() ) :
-		get_template_part( SNIPPETS_DIR . '/post-thumbnail' );
+	if (has_post_thumbnail()) :
+		get_template_part(SNIPPETS_DIR . '/post-thumbnail');
 	endif;
 
-	get_template_part( SNIPPETS_DIR . '/header/entry-header' );
+	get_template_part(SNIPPETS_DIR . '/header/entry-header');
 
-	get_template_part( SNIPPETS_DIR . '/entry-excerpt' );
+	get_template_part(SNIPPETS_DIR . '/entry-excerpt');
 
-	get_template_part( SNIPPETS_DIR . '/post-author' );
+	if (!is_post_type_archive()) :
+		get_template_part(SNIPPETS_DIR . '/post-author');
+	endif;
 
 	?>
 </article>
