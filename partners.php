71fbf462cda6b3bc99d32c35f05ac0a666d051ec
Goce Mitevski <goce.mitevski@keitaro.com>
2020-10-15 18:13:14 +0200

    Re-add deleted partners.php template

---
+++ partners.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Template Name: Partners
+ *
+ * @link https://github.com/keitaroinc/keitaro-theme
+ *
+ * @package WordPress
+ * @subpackage Keitaro
+ */
+
+get_template_part('page');
