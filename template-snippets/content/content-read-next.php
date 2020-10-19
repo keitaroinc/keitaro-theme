5acccf8e7f36816a8805eb6e1eab4b4c391490c5
Nikola Lekich <you@example.com>
2020-10-19 13:20:54 +0200

    Removed the author and the category in the readnext section if the post type belogns to showcases or job-application

--- template-snippets/content/content-read-next.php
+++ template-snippets/content/content-read-next.php
@@ -13,14 +13,17 @@
 <div class="col-lg-4">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
+	$post_type = get_post_type( get_the_ID() );
 
 	if ( has_post_thumbnail() ) :
 		get_template_part( SNIPPETS_DIR . '/post-thumbnail' );
 	endif;
 
 	get_template_part( SNIPPETS_DIR . '/header/entry-header-read-next' );
-
-	get_template_part( SNIPPETS_DIR . '/post-author' );
+	
+	if ($post_type!== 'job-applications' && $post_type !== 'showcases'):
+		get_template_part( SNIPPETS_DIR . '/post-author' );
+	endif;
 
 	?>
 </article>
--- template-snippets/header/entry-header-read-next.php
+++ template-snippets/header/entry-header-read-next.php
@@ -13,8 +13,10 @@
 <header class="entry-header">
 	<?php
 
+$post_type = get_post_type( get_the_ID() );
+if ($post_type!== 'job-applications' && $post_type !== 'showcases'):
 	the_category();
-
+endif;
 	the_title( '<h3 class="entry-title h4"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 
 	?>
