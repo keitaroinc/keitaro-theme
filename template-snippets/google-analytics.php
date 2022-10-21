a89ae9a39b6b2d76f6acc6ddcd259a85713cbed2
Goce Mitevski <goce.mitevski@keitaro.com>
2022-10-21 11:56:21 +0200

    Update Google Analytics tracking code to use gtag.js

--- template-snippets/google-analytics.php
+++ template-snippets/google-analytics.php
@@ -13,22 +13,14 @@ $ga_tracking_code = get_option( 'keitaro_settings' )['ga_tracking_id'] ?? false;
 if ( $ga_tracking_code ) :
 
 	?>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_js( $ga_tracking_code ); ?>"></script>
 	<script>
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-				(i[r].q = i[r].q || []).push(arguments)
-			}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-					m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-		ga('create', '<?php echo esc_js( $ga_tracking_code ); ?>', 'auto');
-		ga('send', 'pageview');
-
+	gtag('config', '<?php echo esc_js( $ga_tracking_code ); ?>');
 	</script>
 	<?php
 
