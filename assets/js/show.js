438de45828b7eedd58dccfc9edadb94217d5a68a
Goce Mitevski <goce.mitevski@keitaro.com>
2023-11-24 09:17:27 +0100

    Remove unused JS assets

--- assets/js/show.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function () {
-  $('.radio-show').click(function () {
-    var inputValue = $(this).attr("value");
-    var targetBox = $("." + inputValue);
-    $(".radio-show-content").not(targetBox).hide();
-    $(targetBox).show("1000");
-  });
-});
--- assets/js/showjobs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('.job-button').click(function () {
-    $("i", this).toggleClass("fa-angle-down fa-angle-up")
-    $(this).toggleClass("job-button job-button-active")
-    var inputValue = $(this).attr("value");
-    var targetBox = $('.' + inputValue);
-    $(targetBox).toggle('1000');
-  })
-})
