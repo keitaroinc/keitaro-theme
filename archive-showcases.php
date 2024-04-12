eba8a081ed94232df7268ac341983e4aa1b362f4
Goce Mitevski <goce.mitevski@keitaro.com>
2024-04-12 10:09:26 +0200

    Add archive template for Showcases

---
+++ archive-showcases.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Template for all pages, when not overridden
+ *
+ * @link https://github.com/keitaroinc/keitaro-theme
+ *
+ * @package WordPress
+ * @subpackage Keitaro
+ */
+
+get_header();
+
+if ( have_posts() ) :
+	$first = true;
+?>
+
+	<?php get_template_part( SNIPPETS_DIR . '/header/page-header-with-background' ); ?>
+
+	<div class="container-fluid">
+
+		<div id="primary" class="content-area">
+
+			<div class="row">
+
+				<div class="col-lg-8 offset-lg-2">
+
+					<main id="main" class="site-main" role="main">
+						<div class="row row-cols-1 row-cols-lg-3">
+							<?php
+
+							while ( have_posts() ) :
+
+								the_post();
+								?>
+								<div class="col">
+									<?php get_template_part( SNIPPETS_DIR . '/content/content-grid' ); ?>
+								</div>
+
+							<?php endwhile; ?>
+						</div>
+					</main>
+
+					<nav class="nav-pagination">
+						<?php
+						get_template_part( SNIPPETS_DIR . '/navigation/pagination' );
+						?>
+					</nav>
+
+				</div>
+
+			</div>
+
+			<?php get_template_part( SNIPPETS_DIR . '/cta-connect' ); ?>
+
+		<?php
+
+	else :
+
+		get_template_part( SNIPPETS_DIR . '/content/content-none' );
+
+	endif;
+		?>
+
+		</div>
+
+	</div>
+
+	<?php
+
+	get_footer();
